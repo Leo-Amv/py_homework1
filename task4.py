e69ac583be0839f89951964b7656e6c2d638b7ca
Leo_Amv <leonidamvrosov@yandex.ru>
2022-09-03 23:55:41 +0500

    task4 completed!

--- task4.py
+++ task4.py
@@ -1,1 +1,15 @@
 # Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+try:
+    n = int(input('\nEnter coordinate plane quarter number from 1 to 4:\t'))
+    if n == 1:
+        print('\nRange of possible coordinates:\n\tx > 0 (from 0 to + ∞)\n\ty > 0 (from 0 to + ∞)\n')
+    elif n == 2:
+        print('\nRange of possible coordinates:\n\tx < 0 (from - ∞ to 0)\n\ty > 0 (from 0 to + ∞)\n')
+    elif n == 3:
+        print('\nRange of possible coordinates:\n\tx < 0 (from - ∞ to 0)\n\ty < 0 (from - ∞ to 0)\n')
+    elif n == 4:
+        print('\nRange of possible coordinates:\n\tx > 0 (from 0 to + ∞)\n\ty < 0 (from - ∞ to 0)\n')
+    else:
+        print('\nYou must enter number from 1 to 4 ! Try again.\n')
+except ValueError:
+    print('\nIncorrect data, you must enter number, try again!\n')
