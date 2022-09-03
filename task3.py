8d41a88976a76b476d2bb29a3ac454fbc723dc45
Leo_Amv <leonidamvrosov@yandex.ru>
2022-09-03 23:13:29 +0500

    try except added task3 completed!

--- task3.py
+++ task3.py
@@ -1,18 +1,20 @@
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
 print('\nCoordinates X and Y must be non-zero\n')
+try:
+    x = int(input('Enter coordinate X:\t'))
+    y = int(input('Enter coordinate Y:\t'))
 
-x = int(input('Enter coordinate X:\t'))
-y = int(input('Enter coordinate Y:\t'))
-
-n = 0
-if x > 0 and y > 0:
-    n = 1
-elif x < 0 and y > 0:
-    n = 2
-elif x < 0 and y < 0:
-    n = 3
-elif x > 0 and y < 0:
-    n = 4
-else:
-    print('\nOne of the coordinates is 0 ! Try again.')
-print(f'\nCoordinate plane quarter number: {n}\n')
+    n = 0
+    if x > 0 and y > 0:
+        n = 1
+    elif x < 0 and y > 0:
+        n = 2
+    elif x < 0 and y < 0:
+        n = 3
+    elif x > 0 and y < 0:
+        n = 4
+    else:
+        print('\nOne of the coordinates is 0 ! Try again.')
+    print(f'\nCoordinate plane quarter number: {n}\n')
+except ValueError:
+    print('\nIncorrect data, you must enter number, try again!\n')
